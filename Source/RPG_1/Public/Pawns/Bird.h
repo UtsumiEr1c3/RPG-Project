b45bba778c6d684c9c0fb90b94e261b03424c934
UtsumiEr1c3 <glhf2233@163.com>
2025-01-23 23:41:08 +0800

    add rotation to controller

--- Source/RPG_1/Public/Pawns/Bird.h
+++ Source/RPG_1/Public/Pawns/Bird.h
@@ -30,6 +30,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	void MoveForward(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
 
 private:
 	UPROPERTY(VisibleAnywhere)
